266ccfec8c7c1e20d0b5c7a1d38960b9c95ad565
root <root@vivien-All-Series>
2019-01-12 17:50:00 +0100

    #1 first commit

---
+++ myMediaCenter.client/src/main/java/lavive/myMediaCenter/client/Client.java
@@ -0,0 +1,18 @@
+package lavive.myMediaCenter.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+        Application.launch(Client.class, args);
+	}
+
+}
